fb79cd6d64fb54371a47d4385ad49057b53b2cfe
Dario <darioo_dario@hotmail.com>
2021-10-20 14:12:46 -0300

    autenticação

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,13 +1,49 @@
-import styles from './styles.module.scss';
-import {VscGithubInverted} from 'react-icons/vsc';
+import styles from "./styles.module.scss";
+import { VscGithubInverted } from "react-icons/vsc";
+import { useEffect } from "react";
+import { api } from "../../services/api";
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id:string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
 
 export function LoginBox() {
-  return <div className={styles.loginBoxWrapper}>
-    <strong>Entre e compartilhe sua mensagem</strong>
-    <a href="#" className={styles.signInWithGithub}>
-      <VscGithubInverted size="24"/>
-      Entrar com github
-    </a>
-  </div>;
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=ebe2002f5c8555f01004`;
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+    const {token, user} = response.data;
+    localStorage.setItem('@dowhile:token', token)
+    console.log(user);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWithCode, githubCode] = url.split("?code=");
+      console.log({ urlWithCode, githubCode });
+      window.history.pushState({}, "", urlWithCode);
+
+      signIn(githubCode);
+    }
+  }, []);
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com github
+      </a>
+    </div>
+  );
 }
